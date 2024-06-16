7ce61eb801369c8b2e7a77116dc98ddce8ec3eb5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-06-16 16:30:35 -0300

    update brave-browser and gaming scripts

--- apps/bottles.sh
+++
@@ -1,5 +0,0 @@
-echo_doing 'Installing Bottles'
-
-flatpak install -y com.usebottles.bottles
-
-echo_done
--- apps/gaming.sh
+++ apps/gaming.sh
@@ -1,6 +1,6 @@
 if [ "$installation_type" == "Desktop" ]; then
 	echo_doing 'Installing Steam'
-	sudo flatpak install com.valvesoftware.Steam -y
+	flatpak install com.valvesoftware.Steam -y
 	sudo nala install steam-devices -y
 	echo_done
 
@@ -10,5 +10,9 @@ if [ "$installation_type" == "Desktop" ]; then
 
 	echo_doing 'Installing Duckstation'
 	flatpak install duckstation -y
+	echo_done
+
+	echo_doing 'Installing Bottles'
+	flatpak install -y com.usebottles.bottles
 	echo_done
 fi
