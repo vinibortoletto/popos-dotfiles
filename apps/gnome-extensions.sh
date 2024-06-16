6afc1abe53a44fae2cad94f818cdd32609085cf3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-06-16 14:03:15 -0300

    update gnome-extension script

--- apps/gnome-extensions.sh
+++ apps/gnome-extensions.sh
@@ -7,4 +7,22 @@ gext install just-perfection-desktop@just-perfection
 gext install space-bar@luchrioh
 gext install vitals@CoreCoding.com
 
+# Vitals dependencies
+sudo nala install -y gir1.2-gtop-2.0 lm-sensors
+
+# Compile gsettings schemas in order to be able to set them
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com/schemas/org.gnome.shell.extensions.vitals.gschema.xml /usr/share/glib-2.0/schemas/ 
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Just Perfection settings
+gsettings set org.gnome.shell.extensions.just-perfection top-panel-position 1
+gsettings set org.gnome.shell.extensions.just-perfection clock-menu-position 1
+gsettings set org.gnome.shell.extensions.just-perfection clock-menu-position-offset 20
+gsettings set org.gnome.shell.extensions.just-perfection notification-banner-position 5
+
+# Vitals settings
+gsettings set org.gnome.shell.extensions.vitals position-in-panel 0
+
 echo_done
