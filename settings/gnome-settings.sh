c3032f104b0eda5e0b43ab9dff73d310efbaa6ae
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-06-16 17:09:29 -0300

    update gnome settings

--- settings/gnome-settings.sh
+++ settings/gnome-settings.sh
@@ -9,4 +9,8 @@ gsettings set org.gnome.desktop.interface enable-animations false
 # Set focus on hover
 gsettings set org.gnome.desktop.wm.preferences focus-mode mouse
 
+# Set background
+gsettings set org.gnome.desktop.background picture-uri file:///home/vini/Pictures/backgrounds/conscious-design-mpRtviiLfMk-unsplash.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file:///home/vini/Pictures/backgrounds/conscious-design-mpRtviiLfMk-unsplash.jpg
+
 echo_done
