eb5c9305554516264351c88f0c67fda43f2a29b1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-10-17 19:06:20 -0300

    update stremio script

--- apps/stremio.sh
+++ apps/stremio.sh
@@ -1,6 +1,9 @@
 echo_doing 'Installing Stremio'
 
 flatpak install -y com.stremio.Stremio
+
+# Sets qBittorrent as default torrent handler
 gio mime x-scheme-handler/magnet org.qbittorrent.qBittorrent.desktop
+gio mime application/x-bittorrent org.qbittorrent.qBittorrent.desktop
 
 echo_done
