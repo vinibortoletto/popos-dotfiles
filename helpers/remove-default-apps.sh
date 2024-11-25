1c82cdfca5b0273b49ade1c141893717905082d1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-11-25 17:14:06 -0300

    sript for removing libreoffice

--- helpers/remove-default-apps.sh
+++ helpers/remove-default-apps.sh
@@ -1,22 +1,16 @@
 echo_doing 'Removing default apps'
 
-sudo apt purge -y 
+sudo apt purge -y \ 
 	firefox \
 	geary \
 	eog \
 	totem \
 	pop-shop 
+	libreoffice-core \
+	libreoffice-common
 
 rm -rf ~/.mozilla
 rm -rf ~/.cache/mozilla
-
-echo_done
-
-
-
-echo_doing 'Removing Libre Office'
-
-sudo apt purge -y libreoffice-core libreoffice-common
 
 echo_done
 
