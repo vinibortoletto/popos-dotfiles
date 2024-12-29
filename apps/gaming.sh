f1486b183b281b2038715f84721cb490924752ef
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-12-29 18:42:01 -0300

    add goverlay script

--- apps/gaming.sh
+++ apps/gaming.sh
@@ -1,4 +1,8 @@
 if [ "$installation_type" == "Desktop" ]; then
+	echo_doing 'Installing GOverlay'
+	sudo nala install goverlay -y
+	echo_done
+
 	echo_doing 'Installing Steam'
 	sudo nala install steam steam-devices -y
 	echo_done
