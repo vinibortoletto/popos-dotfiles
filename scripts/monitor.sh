259cca8b88fffdbdbe29b3454872e0ac54dafe4e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2025-02-05 18:28:58 -0300

    update monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,4 +4,4 @@
 xrandr --output HDMI-0 --mode 1600x900 --rate 60.00 --pos 0x0
 
 # Set resolution for HDMI-1 (primary display)
-xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 60.00 --pos 1600x0
+xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 75.00 --pos 1600x0
